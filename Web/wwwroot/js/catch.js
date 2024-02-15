79b31f818546b40500be14aae1697bcca4cb0cb6
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-02-15 11:43:55 +0100

    Adding Edit and Delete buttons to JS DataTable

--- Web/wwwroot/js/catch.js
+++ Web/wwwroot/js/catch.js
@@ -12,7 +12,15 @@ function loadDataTable() {
             { data: 'species', "width": "15%" },
             { data: 'length', "width": "10%" },
             { data: 'weight', "width": "10%" },
-            { data: 'session.sessionName', "width": "15%" }
+            { data: 'session.sessionName', "width": "15%" },
+            { data: 'id',
+            "render": function(data){
+                return `<div class="w-75 btn-group" role="group">
+                <a href="/customer/catch/edit?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
+                <a href="/customer/catch/delete?id=${data}" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
+                </div>`
+            },
+            "width": "20%" },
         ]
     });
 }

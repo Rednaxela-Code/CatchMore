805c5ac3c93784be3d668e5edec95c4d182f4e12
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-02-15 14:17:08 +0100

    Update to .Net7 and Scaffold Identity pages

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using CatchMore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CatchMore.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -12,20 +13,5 @@ namespace CatchMore.DataAccess.Data
         //DB Tables
         public DbSet<Session> Sessions { get; set; }
         public DbSet<Catch> Catches { get; set; }
-
-        //DB Seed Data
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Session>().HasData(
-        //        new Session { Id = 10, Date = DateTime.Now, Latitude = 51.98807, Longitude = 6.004520 },
-        //        new Session { Id = 11, Date = DateTime.Now, Latitude = 52.98807, Longitude = 6.204520 }
-        //        );
-
-        //    modelBuilder.Entity<Catch>().HasData(
-
-        //        new Catch() { Id = 2, Date = DateTime.Now, Species = "Perch", Weight = 2, Length = 50, SessionId = 10, Image = ""},
-        //        new Catch() { Id = 3, Date = DateTime.Now, Species = "Perch", Weight = 1.5, Length = 45, SessionId = 11, Image = "" }
-        //        );
-        //}
     }
 }
--- Web/Program.cs
+++ Web/Program.cs
@@ -2,6 +2,7 @@ using CatchMore.DataAccess.Data;
 using CatchMore.DataAccess.Repository;
 using CatchMore.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(o => 
 o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 

68b3544392f94bf2b5c9afd43bf148536de4663a
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-02-16 16:28:17 +0100

    Only Admin can create Admin 2

--- Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -173,7 +173,14 @@ namespace Web.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+                        {
+                            TempData["success"] = "New user created!";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }

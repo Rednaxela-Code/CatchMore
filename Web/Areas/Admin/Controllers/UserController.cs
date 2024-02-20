8de7563949118b45809525b192b9792371e7d131
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-02-20 14:39:32 +0100

    UserController and Index Added to Project
    Changed _Layout and added GetAll Endpoint in Controller

---
+++ Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using CatchMore.DataAccess.Repository.IRepository;
+using CatchMore.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using CatchMore.Models;
+
+
+namespace Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _unitOfWork.ApplicationUser.GetAll().ToList();
+            return Json(new { data = objUserList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            //_unitOfWork.ApplicationUser.Remove();
+            return Json(new { success = true, message = "Delete Successfull" });
+        }
+        #endregion
+    }
+}
---
+++ Web/wwwroot/js/user.js
@@ -0,0 +1,25 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        "columns": [
+            { data: 'userName', "width": "15%" },
+            { data: 'email', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                <a href="/customer/catch/edit?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
+                </div>`
+                },
+                "width": "20%"
+            },
+        ]
+    });
+}
+

2d0ac8bd727c2f85e21d3dd227672d9b513bfe2a
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-02-27 15:32:33 +0100

    Starting build on some Unit tests
    This the first time i test some code, very new to this and do not know exactly what i should want to need to test...

---
+++ CatchMore.Tests/SessionControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using FluentAssertions;
+using Web.Areas.Customer.Controllers;
+using CatchMore.Models;
+using CatchMore.DataAccess.Repository.IRepository;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatchMore.Tests
+{
+    public class SessionControllerTests
+    {
+        [Fact]
+        public void CreateAction_ReturnsRedirectOnSuccess()
+        {
+            //Arrange
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var controller = new SessionController(mockUnitOfWork.Object);
+            var model = new Session
+            {
+                SessionName = "Test Session",
+                Date = DateTime.Now,
+                Latitude = 10,
+                Longitude = 11,
+            };
+
+            //Act
+            var result = controller.Create(model);
+
+            //Assert
+            result.Should().BeOfType<RedirectToActionResult>()
+                  .Which.ActionName.Should().Be("Index");
+        }
+    }
+}
